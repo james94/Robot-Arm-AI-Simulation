54d0726616150fa026a1c8edd203ea7e04035bf8
james94 <jamesmedel94@gmail.com>
2024-11-10 21:36:02 -0800

    Figured out how to download Unity ur3_moveit package
    By cloning Unity's https://github.com/Unity-Technologies/Robotics-Object-Pose-Estimation.git
    
    Then we copy out the ur3_moveit to ROS_WORKSPACE/src and we delete the
    Robotics-Object-Pose-Estimation.
    
    I was able to build docker image successfully.
    
    The only dependence from ROS/src comes from pre-trained UR3_single_cube_model.tar
    inside ur3_moveit. In the next commit, I will try push that model.

---
+++ docker/unity-robotics/scripts/comp/50-clone-unity_ur3_moveit.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+git clone https://github.com/Unity-Technologies/Robotics-Object-Pose-Estimation.git $ROS_WORKSPACE/src/Robotics-Object-Pose-Estimation
+
+if [ $? -eq 0 ]; then
+    echo "Cloned Robotics-Object-Pose-Estimation repository"
+else
+    echo "Failed to clone Robotics-Object-Pose-Estimation repository"
+    exit 1
+fi
+
+pushd $ROS_WORKSPACE/src/Robotics-Object-Pose-Estimation/ROS/src/
+
+cp -r ur3_moveit $ROS_WORKSPACE/src/ur3_moveit
+
+echo "Copied Unity's ur3_moveit package to ROS workspace"
+
+rm -rf $ROS_WORKSPACE/src/Robotics-Object-Pose-Estimation
+
+echo "Removed Unity's Robotics-Object-Pose-Estimation package"
+
+popd
