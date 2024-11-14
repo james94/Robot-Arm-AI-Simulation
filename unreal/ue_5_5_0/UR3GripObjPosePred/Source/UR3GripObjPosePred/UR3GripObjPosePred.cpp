70c3a541468cffe069f360d14a48682b232abd71
james94 <jamesmedel94@gmail.com>
2024-11-14 08:05:03 -0800

    Created Unreal Engine 5.5.0 Basis Project for UR3 Grip Obj Pose Pred

---
+++ unreal/ue_5_5_0/UR3GripObjPosePred/Source/UR3GripObjPosePred/UR3GripObjPosePred.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UR3GripObjPosePred.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UR3GripObjPosePred, "UR3GripObjPosePred" );
